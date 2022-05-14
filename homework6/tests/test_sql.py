53697a489da712bc32cde648df5d64c3f23fc47f
Stepanov Nikita <n.stepanov@corp.mail.ru>
2022-05-15 01:14:35 +0300

    added a new way to extract data from db

--- homework6/tests/test_sql.py
+++ homework6/tests/test_sql.py
@@ -12,24 +12,15 @@ class TestCountRequests(MySqLTest):
         self.total_requests_from_parse = count_requests(self.mysql_builder, path)
         self.mysql.session.commit()
 
-        self.count_parsed = 1
-        self.lines_from_db = self.mysql.session.query(CountRequests).all()
-
-        self.total_requests_from_db = str(self.mysql.session.query(CountRequests).get('1')).split(':')[1]
-        self.total_requests_from_db_int = int(self.total_requests_from_db.replace('"', '').replace("'", ''))
+        self.count_parsed = 225132
+        self.lines_from_db = self.mysql.session.query(CountRequests).first()
 
     @pytest.mark.SQL
     def test_count_total_number_of_request(self):
-        assert len(self.lines_from_db) == self.count_parsed
-        assert self.total_requests_from_parse == self.total_requests_from_db_int
+        assert self.count_parsed == self.lines_from_db.count
 
 
 class TestCountRequestsType(MySqLTest):
-
-    def parse_type_of_request(self, index):
-        total_req_defined_type_from_db = str(self.mysql.session.query(CountRequestsType).get(str(index))).split(':')[1]
-        total_req_defined_type_from_db_int = int(total_req_defined_type_from_db.replace('"', '').replace("'", ''))
-        return total_req_defined_type_from_db_int
 
     def prepare(self, path):
         self.mysql.create_table('count_requests_type')
@@ -39,21 +30,21 @@ class TestCountRequestsType(MySqLTest):
         self.count_parsed = len(self.total_requests_by_type_from_parse)
         self.lines_from_db = self.mysql.session.query(CountRequestsType).all()
 
-        self.total_get_requests_from_db = self.parse_type_of_request(1)
+        self.total_get_requests_from_db = self.mysql.session.query(CountRequestsType).filter_by(type="GET").first()
 
-        self.total_post_requests_from_db = self.parse_type_of_request(2)
+        self.total_post_requests_from_db = self.mysql.session.query(CountRequestsType).filter_by(type="POST").first()
 
-        self.total_put_requests_from_db = self.parse_type_of_request(3)
+        self.total_put_requests_from_db = self.mysql.session.query(CountRequestsType).filter_by(type="PUT").first()
 
-        self.total_head_requests_from_db = self.parse_type_of_request(4)
+        self.total_head_requests_from_db = self.mysql.session.query(CountRequestsType).filter_by(type="HEAD").first()
 
     @pytest.mark.SQL
     def test_count_total_requests_separate_by_type(self):
         assert len(self.lines_from_db) == self.count_parsed
-        assert self.total_get_requests_from_db == self.total_requests_by_type_from_parse["GET"]
-        assert self.total_post_requests_from_db == self.total_requests_by_type_from_parse["POST"]
-        assert self.total_put_requests_from_db == self.total_requests_by_type_from_parse["PUT"]
-        assert self.total_head_requests_from_db == self.total_requests_by_type_from_parse["HEAD"]
+        assert self.total_get_requests_from_db.count == self.total_requests_by_type_from_parse["GET"]
+        assert self.total_post_requests_from_db.count == self.total_requests_by_type_from_parse["POST"]
+        assert self.total_put_requests_from_db.count == self.total_requests_by_type_from_parse["PUT"]
+        assert self.total_head_requests_from_db.count == self.total_requests_by_type_from_parse["HEAD"]
 
 
 class TestCountTopResource(MySqLTest):
