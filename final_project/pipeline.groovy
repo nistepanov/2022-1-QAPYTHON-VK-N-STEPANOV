c7bd41ae7bb7211b83ec915466a4df49273b1e1f
Stepanov Nikita <n.stepanov@corp.mail.ru>
2022-05-22 18:53:55 +0300

    added file for pipeline

---
+++ final_project/pipeline.groovy
@@ -0,0 +1,40 @@
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    agent {
+        node {
+            label 'master'
+        }
+    }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '3'))
+        timestamps()
+    }
+
+    stages {
+
+         stage("Preparing") {
+            steps {
+                echo "one"
+                sh "cd $WORKSPACE/final_project && pip install -r requirements.txt"
+            }
+
+        stage("Building...") {
+            steps {
+                echo "two"
+                sh "cd $WORKSPACE/final_project && docker-compose up -d"
+            }
+        }
+    }
+
+    post {
+        always {
+            allure([
+                reportBuildPolicy: 'ALWAYS',
+                results: [[path: 'alluredir']]
+            ])
+            cleanWs()
+        }
+    }
+}
